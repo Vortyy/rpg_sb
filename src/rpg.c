795dfc4a2b9737238682803bcb3e5210687a95ea
vorty <uf4vortex@gmail.com>
2025-02-18 23:43:03 +0100

    testing

--- src/rpg.c
+++ src/rpg.c
@@ -7,46 +7,58 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <sqlite3.h>
 
-sqlite3 *db; /* database ptr */
+#define NAMES_SIZE 8
+#define MAX_LVL 8 
 
-int test_callback(void *ptr, int nb_col, char **col_values, char **col_names){
-  for(int i = 0; i < nb_col; i++){
-    printf("| %s : %s |", *(col_names + i), *(col_values + i));  
-  }
-  printf("\n");
-  return 0;
-}
+#define TEAM_SIZE 3
+#define STATS_NUM 6 /* 0:HP, 1:MP, 2:ATK, 3:MATK, 4:DEF, 5:MDEF */ 
 
-void generate_account(int n){
-  while(n-- > 0){
-    char *str;
-    int id = rand() % 1000;
-    sprintf(str, "insert into account(account_id, account_name) values ('%d', 'vorty_%d');", id, id);
-    sqlite3_exec(db, str, NULL, NULL, NULL);
-  }
+#define print_entity(e) printf(" ------------------- \n - name : %s \n - lvl : %d (.xp:%d/%d) \n - stats: [%d,%d,%d,%d,%d,%d] \n", \
+    e.name, e.level, e.xp, xp_required[e.level], \
+    e.base_stats[0], e.base_stats[1], e.base_stats[2], e.base_stats[3], e.base_stats[4], e.base_stats[5])
+
+int xp_required[MAX_LVL] = {0, 100, 200, 400, 800, 1600, 3200, 6400}; 
+
+char *entity_names[NAMES_SIZE] = {
+  "Rohan",
+  "Vorty",
+  "Mickeal",
+  "Trevor",
+  "Anastazia",
+  "Elizabeth",
+  "Rotomator",
+  "Cumcumbre"
+};
+
+typedef struct _entity {
+  char *name;
+  int xp;
+  int level;
+  int base_stats[STATS_NUM];
+} entity;
+
+/* generate_entity : generate random entity base on seed from srand */
+entity generate_entity(){
+  entity e = {
+    .name = entity_names[rand() % NAMES_SIZE],
+    .xp = 0,
+    .level = 1,
+    .base_stats = {rand() % 20, rand() % 20, rand() % 20, rand() % 20, rand() % 20, rand() % 20}
+  };
+
+  return e;
 }
 
 int main(){
 
-  srand(time(NULL));
-
-  if(sqlite3_open("./src/rpg.db", &db) == SQLITE_OK){
-    printf("db opened ready to be queried\n");
-
-    generate_account(5);
-    //do query
-    sqlite3_exec(
-        db,
-        "select * from account;",
-        test_callback,
-        NULL,
-        NULL);
-
-    sqlite3_close(db);
-    exit(0);
+  srand(time(NULL)); /* seed fix but can be randomised by time(NULL) */
+  entity team[TEAM_SIZE];
+  
+  for (int i = 0; i < TEAM_SIZE; i++){
+    team[i] = generate_entity();
+    print_entity(team[i]);
   }
 
-  return 1;
+  return 0;
 }
